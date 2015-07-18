cc1d9daa9c078eb8080fb2dd2f1088c3cba6c0b3
Tonytino <tony91139@Gmail.com>
2015-07-18 14:31:23 -0700

    Create fake login/out functionality and demo  handlebars use

--- js/multi-event.js
+++ js/multi-event.js
@@ -4,12 +4,31 @@
 // the context variable should hold an array of json objects representing events
 // and would require at least the details in the example below
 
-var source      = $("#multi-event-template").html();
-var template    = Handlebars.compile(source);
-var context     = [{
-                    title: "Event Title",
-                    location: "Sunnyvale, CA",
-                    image: "http://www.officialpsds.com/images/stocks/Concert-stock3112-large.png",
-                    date: "July 1, 2015"
-                }];
-var html        =  template(context);
+var populateMultiEventView = function() {
+    var source      = $("#multi-event-template").html();
+    var template    = Handlebars.compile(source);
+    var context     = {
+                        events: [{
+                                    title: "Event Title",
+                                    location: "Sunnyvale, CA",
+                                    image: "http://www.officialpsds.com/images/stocks/Concert-stock3112-large.png",
+                                    date: "July 1, 2015"
+                                },
+                                {
+                                    title: "Event Title",
+                                    location: "Sunnyvale, CA",
+                                    image: "http://www.officialpsds.com/images/stocks/Concert-stock3112-large.png",
+                                    date: "July 1, 2015"
+                                },
+                                {
+                                    title: "Event Title",
+                                    location: "Sunnyvale, CA",
+                                    image: "http://www.officialpsds.com/images/stocks/Concert-stock3112-large.png",
+                                    date: "July 1, 2015"
+                                }]
+                    };
+    var html        =  template(context);
+
+    $('.container').html(html);
+}
+
